c48cad313fe6da9327a9fc7279710b1620ae9962
Srpopty <Srpopty@outlook.com>
2017-08-30 11:24:26 +0800

    aaaaaa
    aaaaaa

--- player.h
+++ player.h
@@ -1,29 +1,29 @@
-#ifndef PLAYER_H_INCLUDED
-#define PLAYER_H_INCLUDED
+#ifndef PLAYER1_H_INCLUDED
+#define PLAYER1_H_INCLUDED
 
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
-class Player
-{
-private:
-    double prestige;
-    double bitcoin;
-    double violence;
-    int second;
-    int war_num;
-    friend class User;
-public:
-    Player ( );
-
-    void TrainCoder ( );
-    void DigMine ( );
-    void WashBrain ( );
-    void StartWar ( );
+class Player{
+    private:
+        double prestige;
+        double bitcoin;
+        double violence;
+        int second;
+        int warnum;
+        friend class User;
+    public:
+        Player();
+        void showStatus();
+        void TrainCoder();
+        void DigMine();
+        void WashBrain() ;
+        ~Player(){};
 };
 
-#endif // PLAYER_H_INCLUDED
+#endif // PLAYER1_H_INCLUDED
